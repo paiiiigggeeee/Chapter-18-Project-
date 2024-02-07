f913559f2ffeed016b0fa8a8c55b887a3ee2da20
paiiiigggeeee <91573998+paiiiigggeeee@users.noreply.github.com>
2024-02-07 10:23:20 -0600

    Add files via upload

---
+++ frmInvMaint.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace InventoryMaintenance
+{
+    partial class frmInvMaint
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstItems = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cboFilterBy = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstItems
+            // 
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.ItemHeight = 15;
+            this.lstItems.Location = new System.Drawing.Point(19, 55);
+            this.lstItems.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(306, 154);
+            this.lstItems.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(348, 55);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 25);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add Item";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(348, 92);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 25);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Item";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(348, 128);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 25);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cboFilterBy
+            // 
+            this.cboFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFilterBy.FormattingEnabled = true;
+            this.cboFilterBy.Location = new System.Drawing.Point(65, 13);
+            this.cboFilterBy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboFilterBy.Name = "cboFilterBy";
+            this.cboFilterBy.Size = new System.Drawing.Size(129, 23);
+            this.cboFilterBy.TabIndex = 1;
+            this.cboFilterBy.SelectedIndexChanged += new System.EventHandler(this.cboFilterBy_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Show:";
+            // 
+            // frmInvMaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(476, 233);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboFilterBy);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstItems);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmInvMaint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inventory Maintenance";
+            this.Load += new System.EventHandler(this.frmInvMaint_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstItems;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cboFilterBy;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ frmInvMaint.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryMaintenance
+{
+    public partial class frmInvMaint : Form
+    {
+        public frmInvMaint()
+        {
+            InitializeComponent();
+        }
+
+        private List<InvItem> invItems = null;
+
+        private void frmInvMaint_Load(object sender, EventArgs e)
+        {
+            invItems = InvItemDB.GetItems();
+            LoadComboBox();
+            FillItemListBox();
+        }
+
+        private void LoadComboBox()
+        {
+            cboFilterBy.DataSource = new string[] {
+                "All", "Under $10", "$10 to $50", "Over $50"
+            };
+        }
+
+        private void FillItemListBox()
+        { 
+            lstItems.Items.Clear();
+            
+            string filter = cboFilterBy.SelectedValue.ToString();
+            IEnumerable<InvItem> filteredItems = null;
+
+            // add items to the filteredItems collection based on FilterBy value
+
+            if (filter == "Under $10")
+
+            {
+
+                filteredItems = invItems.Where(x => x.Price < 10);
+
+            }
+
+            else if (filter == "$10 to $50")
+
+            {
+
+                filteredItems = invItems.Where(x => x.Price >= 10 && x.Price <= 50);
+
+            }
+
+            else if (filter == "Over $50")
+
+            {
+
+                filteredItems = invItems.Where(x => x.Price > 50);
+
+            }
+
+            else // all
+
+            {
+
+                filteredItems = invItems;
+
+            }
+
+           
+
+
+            // change code to loop the filteredItems collection
+            foreach (InvItem item in filteredItems)
+            {
+                lstItems.Items.Add(item.DisplayText);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmNewItem newItemForm = new frmNewItem();
+            InvItem invItem = newItemForm.GetNewItem();
+            if (invItem != null)
+            {
+                invItems.Add(invItem);
+                InvItemDB.SaveItems(invItems);
+                FillItemListBox();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            
+            
+            int i = lstItems.SelectedIndex;
+            if (i != -1)
+            {
+                //InvItem invItem = (InvItem)invItems[i];
+
+                string displayText = lstItems.Items[i].ToString();
+                InvItem invItem = invItems  //Create LINQ Query to get selected InvItem from InvItems collection
+                                  .Where(item => item.DisplayText == displayText)
+                                  .FirstOrDefault(); //Use firstordefault to use the text value just retrieved
+                 
+                string message = $"Are you sure you want to delete {invItem.Description}?";
+                DialogResult button =
+                    MessageBox.Show(message, "Confirm Delete",
+                    MessageBoxButtons.YesNo);
+                if (button == DialogResult.Yes)
+                {
+                    invItems.Remove(invItem);
+                    InvItemDB.SaveItems(invItems);
+                    FillItemListBox();
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cboFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillItemListBox();
+        }
+    }
+}
